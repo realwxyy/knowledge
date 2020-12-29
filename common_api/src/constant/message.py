b40542966404fc45f8428eda6c5aaf4f96bbba27
realwxyy <realwxyy@126.com>
2020-12-29 14:05:14 +0800

    新增product相关异常提示

--- common_api/src/constant/message.py
+++ common_api/src/constant/message.py
@@ -1,3 +1,8 @@
+# brand
 BRAND_DUPLICATION = '请勿重复添加品牌：'
 BRAND_DEDUPLICATION = '该品牌已被删除，无法重复删除'
 BRAND_INFO_QUERY_FAIL = '品牌信息查询失败，请确认品牌id是否正确'
+
+# product
+PRODUCT_DEDUPLICATION = '该商品已被删除，无法重复删除'
+PRODUT_INFO_QUERY_FAIL = '商品信息查询失败，请确认商品id是否正确'
