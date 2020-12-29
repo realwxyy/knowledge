a1eae1ab1f34b48e14c85d271554a4283aeed3f5
realwxyy <realwxyy@126.com>
2020-12-29 13:42:22 +0800

    方法名太长 换了方法名 后期更换较短的方法名

--- common_api/src/utils/utils.py
+++ common_api/src/utils/utils.py
@@ -1,6 +1,20 @@
 import time
 
 
+def dict_not_empty(params, key_arr):
+    '''
+    @ desc 验证参数是否为空
+    @ param  params 要验证的dict key_arr 要验证的key的list
+    '''
+    msg = []
+    code = 0
+    for k in key_arr:
+        if not all([params.get(k)]):
+            code = -1
+            msg.append('参数 [' + k + '] 不可为空')
+    return {'code': code, 'msg': msg}
+
+
 def validate_dict_not_empty_with_key(params, key_arr):
     '''
     @ desc 验证参数是否为空
